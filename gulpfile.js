a5f72e740ca25d2fe553a886eb10c322e30bc831
bellmounte <ryan.dacquisto@gmail.com>
2015-12-07 21:39:09 -0800

    Initial environment setup.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var jshint = require('gulp-jshint');
+
+var jshint_config = {
+	bitwise: true,
+	browser: true,
+	curly: true,
+	eqeqeq: true,
+	forin: true,
+	immed: true,
+	indent: 4,
+	noarg: true,
+	node: true,
+	noempty: true,
+	nonbsp: true,
+	quotmark: 'single',
+	strict: true,
+	undef: true,
+	unused: true
+};
+
+gulp.task('default', ['html', 'css', 'js'], function() {});
+
+gulp.task('html', function() {
+	gulp.src('src/*.htm*')
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('src/styles/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('public/css'));
+});
+
+gulp.task('jshint', function () {
+	return gulp.src(['src/scripts/*.js', 'src/scripts/**/*.js', 'src/**/*.js'])
+		.pipe(jshint(jshint_config))
+		.pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('js', ['jshint'], function () {
+	return gulp.src('src/scripts/app.js')
+		.pipe(babel())
+		.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('watch', ['default'], function() {
+	gulp.watch('src/*.htm', ['htm']);
+	gulp.watch('src/scripts/**/*.js', ['js']);
+	gulp.watch('src/styles/**/*.scss', ['css']);
+});
